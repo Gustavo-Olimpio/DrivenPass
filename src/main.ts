4b23021e5ee44f45941ab733fa8f681b13980f1a
GustavoOlimpio <gustavo-olimpio@hotmail.com>
2023-09-03 23:27:34 -0300

    feat/swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('DrivenPass - Rest API')
+    .setDescription('DrivenPass API description')
+    .setVersion('1.0')
+    .addTag('DrivenPass')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port, () => {
     console.log(`Server is up and listen on port ${port}`);
   });
