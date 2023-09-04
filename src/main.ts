72651072150ea6f81cd7b0aa185f3b97188578e3
GustavoOlimpio <gustavo-olimpio@hotmail.com>
2023-09-03 23:01:09 -0300

    feat/deploy

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(port, () => {
+    console.log(`Server is up and listen on port ${port}`);
+  });
 }
 bootstrap();
